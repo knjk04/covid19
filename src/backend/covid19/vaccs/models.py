3b97166e0bef54f12d278f3da4ef95d94ba4acec
Karan Kumar <karan@karankumar.com>
2023-07-06 16:24:40 +0100

    chore: init Django project

---
+++ src/backend/covid19/vaccs/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=60)
+    last_updated = models.DateField()
+    vaccine = models.ManyToManyField(Vaccine)
+    source = models.OneToOneField(Source, on_delete=models.PROTECT)
