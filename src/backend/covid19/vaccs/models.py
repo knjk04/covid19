a98ce5232c25bf68345be047f6bb5ec0bff28337
Karan Kumar <karan@karankumar.com>
2023-07-06 16:38:33 +0100

    chore: make names unique

--- src/backend/covid19/vaccs/models.py
+++ src/backend/covid19/vaccs/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 
 class Vaccine(models.Model):
-    name = models.CharField(max_length=100)
+    """
+    Name of a vaccine (e.g. Pfizer/BioNTech)
+    """
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Source(models.Model):
+    """
+    Where the information has come from (e.g. World Health Organization)
+    """
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
@@ -16,7 +22,7 @@ class Source(models.Model):
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, unique=True)
     last_updated = models.DateField()
     vaccine = models.ManyToManyField(Vaccine)
     source = models.OneToOneField(Source, on_delete=models.PROTECT)
