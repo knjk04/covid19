d468508331874ce9993181ec8bcd307bcb938347
Karan Kumar <karan@karankumar.com>
2023-07-06 16:31:06 +0100

    chore: make source name unique

--- src/backend/covid19/vaccs/models.py
+++ src/backend/covid19/vaccs/models.py
@@ -9,7 +9,7 @@ class Vaccine(models.Model):
 
 
 class Source(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
