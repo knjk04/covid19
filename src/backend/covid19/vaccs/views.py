c21fb10433d06ff4bd6082836aec0154249e58c0
Karan Kumar <karan@karankumar.com>
2023-07-07 20:54:19 +0100

    chore: save vaccines

--- src/backend/covid19/vaccs/views.py
+++ src/backend/covid19/vaccs/views.py
@@ -5,7 +5,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Country, Source
+from .models import Country, Source, Vaccine
 from .serializers import CountrySerializer
 from .utils import get_locations_df
 
@@ -18,11 +18,21 @@ class CountryViewSet(viewsets.ModelViewSet):
 @api_view(["POST"])
 def ingest(request):
     for row in get_locations_df().itertuples():
+        vaccines = row.vaccines.split(", ")
+        for v in vaccines:
+            try:
+                Vaccine(name=v).save()
+                print(f"Saved {v}...")
+            except IntegrityError:
+                print(f"{v} exists")
+
         s = Source(name=row.source_name)
         try:
             s.save()
             print(f"Saved {row.source_name}")
         except IntegrityError:
             print(f"{row.source_name} exists")
+
         print()
+
     return Response(status=status.HTTP_200_OK)
