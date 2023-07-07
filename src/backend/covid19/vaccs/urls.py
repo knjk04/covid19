f51fbc7a6897465f1b3681447afcab42a2938bf7
Karan Kumar <karan@karankumar.com>
2023-07-07 20:44:18 +0100

    chore: add ingestion endpoint

--- src/backend/covid19/vaccs/urls.py
+++ src/backend/covid19/vaccs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
@@ -7,7 +8,9 @@ routers = routers.DefaultRouter()
 routers.register(r'countries', views.CountryViewSet)
 
 urlpatterns = [
-    # path("", views.index, name="index")
     path("", include(routers.urls)),
-    path("api", include("rest_framework.urls", namespace="rest_framework"))
+    path("api", include("rest_framework.urls", namespace="rest_framework")),
+    path("ingest", views.ingest)
 ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- src/backend/covid19/vaccs/views.py
+++ src/backend/covid19/vaccs/views.py
@@ -1,9 +1,35 @@
-from rest_framework import viewsets
+import pandas as pd
+from django.db.utils import IntegrityError
+from django.http import HttpResponse
+from rest_framework import status, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-from .models import Country
+from .models import Country, Source
 from .serializers import CountrySerializer
 
 
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+
+
+@api_view(["POST"])
+def ingest(request):
+    for row in get_locations_df().itertuples():
+        s = Source(name=row.source_name)
+        try:
+            s.save()
+            print(f"Saved {row.source_name}")
+        except IntegrityError:
+            print(f"{row.source_name} exists")
+        print()
+    return Response(status=status.HTTP_200_OK)
+
+
+def get_locations_df() -> pd.DataFrame:
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/locations.csv")
+    del df['iso_code']
+    del df["source_website"]
+    return df
